038eb6fd9bcb1340bcfc630c22aebbf28d7b8211
orozmo <32623320+orozmo@users.noreply.github.com>
2017-10-15 09:31:25 -0700

    Homework -FindMax

--- src/FindMax.java
+++ src/FindMax.java
@@ -5,7 +5,17 @@ public class FindMax {
 	 * Define a program that finds the maximum number when 2 numbers are given.
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int x = 3;
+		int y = 4;
+				
+		if (x<y)
+		{
+			System.out.println( "TRUE");
+		}
+		else 
+		{
+			System.out.println( "TRUE");
+		}		
 
 	}
 
