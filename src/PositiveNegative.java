ed48a39f6d48c64e047fcfdfb85971d5900cd461
orozmo <32623320+orozmo@users.noreply.github.com>
2017-10-15 09:47:30 -0700

    Homework -PositiveNegative

--- src/PositiveNegative.java
+++ src/PositiveNegative.java
@@ -6,6 +6,15 @@ public class PositiveNegative {
 	 * The program should print positive if the number is positive, negative if it is negative
 	 */
 	public static void main(String args[]){
-		
+	      int x = -5;
+
+        if(x > 0) 
+        {
+            System.out.println("Number is positive.");
+        }
+        else
+        {
+            System.out.println("Number is negative.");
+        }
 	}
 }
